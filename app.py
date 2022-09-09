2a6bc061187794c6b47ae80ff340c2cbdb161809
PallaviSriva <108968023+PallaviSriva@users.noreply.github.com>
2022-09-09 10:53:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,14 @@ import json
 import urllib
 import re
 
+op=webdriver.ChromeOptions()
+op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+op.add_argument("--headless")
+op.add_argument("--no-sandbox")
+op.add_argument("disable-dev-sh-usage")
+
+driver=webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
+
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])  # route to display the home page

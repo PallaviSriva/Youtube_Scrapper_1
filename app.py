dc88fb0287477056e8886b55120e68208fc0e64d
PallaviSriva <108968023+PallaviSriva@users.noreply.github.com>
2022-09-09 20:59:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,13 +18,13 @@ import urllib
 import re
 import os
 
-# op=webdriver.ChromeOptions()
-# op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-# op.add_argument("--headless")
-# op.add_argument("--no-sandbox")
-# op.add_argument("disable-dev-sh-usage")
+op=webdriver.ChromeOptions()
+op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+op.add_argument("--headless")
+op.add_argument("--no-sandbox")
+op.add_argument("disable-dev-sh-usage")
 
-# driver=webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
+driver=webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
 
 app = Flask(__name__)
 
